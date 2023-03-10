6bb22ba92b465aa3225537a0bd168509e25a1442
Corey Silver <coreysilver@outlook.com>
2023-03-09 22:35:43 -0500

    Added feature which lets admins add sales

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :sales, only: [:index, :new]
   end
 
   get '/login' => 'sessions#new'
