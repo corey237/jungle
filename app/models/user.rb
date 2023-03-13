344e3379cc09c2f2ed9e7262fd6e66903ebf47ea
Corey Silver <coreysilver@outlook.com>
2023-03-13 19:30:19 -0400

    Add unit tests for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
 class User < ApplicationRecord
+  
   has_secure_password
+  validates :email, presence: true, :uniqueness => {:case_sensitive => false} 
+  validates :name, presence: true
+  # validates :password, presence: true, comparison: { equal_to: :password_confirmation }, length: {minimum: 6}
+  validates :password, presence: true, confirmation: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+  
+  
+  def self.authenticate_with_credentials(email, password)
+    email.downcase!
+    email.strip!
+    @user = User.find_by_email(email)
+    if @user == nil
+      return nil
+    end
+    @user.authenticate(password)
+  end 
 end
